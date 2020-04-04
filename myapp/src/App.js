149dc63677353ca3b5d7eb9046ad0fec909a4e4f
titus buchanan <titusbuchananjr@gmail.com>
2020-04-03 20:38:35 -0400

    added a map function

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App</h1>
-      <p>Welcome :)</p>
-      <Ninjas name="Ryu" age="27" belt="black"/>
-      <Ninjas name="Teezus" age="21007" belt="blue"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
 }
 
 export default App;
