ee12f995dae89b4b259dca9ab1845d3a2b6d1adc
titus buchanan <titusbuchananjr@gmail.com>
2020-04-06 23:32:19 -0400

    added a function

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 }, 
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
@@ -18,5 +18,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;
