677e2ac9cd245299e36b9168504b7276b415a61f
titus buchanan <titusbuchananjr@gmail.com>
2020-04-08 18:02:04 -0400

    added if statement

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
-        const {ninjas} = this.props;
+
+const Ninjas = ({ninjas}) => {
+   
         const ninjaList = ninjas.map(ninja =>{
+            if(ninja.age > 20){
             return (
                 <div className="ninja"key={ninja.id}>
-            <div>Name:{ninja.name}</div>
-            <div>Age:{ninja.age}</div>
-            <div>Belt:{ninja.belt}</div>
-            </div>
+                    <div>Name:{ninja.name}</div>
+                    <div>Age:{ninja.age}</div>
+                    <div>Belt:{ninja.belt}</div>
+                </div>
             )
+            } else {
+                return null
+            }
         })
         return(
             <div className="ninja-list">
                 { ninjaList }
             </div>
         )
-    }
+    
 }
 
 export default Ninjas
